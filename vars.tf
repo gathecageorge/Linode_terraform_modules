2069707f722cc829ebc6d1221c10c186056c8854
George Gatheca <george.gatheca@definedcrowd.com>
2022-01-04 19:12:12 +0000

    Initial commit

---
+++ vars.tf
@@ -0,0 +1,17 @@
+# access token to linode account
+variable "token" {
+  description = "access token to linode account"
+  type        = string
+}
+
+# root password to be set on linodes
+variable "instance_root_password" {
+  description = "root password to be set on linodes"
+  type        = string
+}
+
+# instances/machines to create, different configurations
+variable "instance_types" {
+  description = "instances/machines to create, different configurations"
+  type        = map(any)
+}
